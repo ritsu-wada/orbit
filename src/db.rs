142907f74ebe2100fdd960a6f60d119ec4553999
R2 <s1f102503183@iniad.org>
2026-02-25 01:39:23 +0900

    add : 詳細とかの項目を追加

--- src/db.rs
+++ src/db.rs
@@ -3,6 +3,16 @@ use rusqlite::{Connection, Result};
 struct Task {
     id: i32,
     name: String,
+    description: Option<String>,
+    state: State,
+}
+
+#[derive(Debug)]
+enum State {
+    Active,
+    Complite,
+    Untouched,
+    Unknown,
 }
 
 pub fn setup_db() -> Result<Connection> {
@@ -11,6 +21,8 @@ pub fn setup_db() -> Result<Connection> {
         "CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL,
+            discription TEXT,
+            state TEXT NOT NULL
         )",
         (),
     )?;
@@ -23,12 +35,29 @@ pub fn add_data(conn: &Connection, name: String) -> Result<()> {
 }
 
 pub fn get_data(conn: &Connection) -> Result<()> {
-    let mut stmt = conn.prepare("SELECT id, name, FROM tasks")?;
-    let _task_iter = stmt.query_map([], |row| {
+    let mut stmt = conn.prepare("SELECT id, name, discription, state FROM tasks")?;
+    let task_iter = stmt.query_map([], |row| {
         Ok(Task {
             id: row.get(0)?,
             name: row.get(1)?,
+            description: Some(row.get(2)?),
+            state: {
+                let state_str: String = row.get(3)?;
+                match state_str.as_str() {
+                    "active" => State::Active,
+                    "complite" => State::Complite,
+                    "Untouched" => State::Untouched,
+                    _ => State::Unknown,
+                }
+            },
         })
     })?;
+    for task in task_iter {
+        let t = task?;
+        println!("ID: {} Name: {} State: {:?}", t.id, t.name, t.state);
+        if let Some(desc) = t.description {
+            println!("Description: /n {}", desc)
+        }
+    }
     Ok(())
 }
